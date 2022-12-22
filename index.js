796a4632043f4db0572e4dc050773d96ad5480e2
Ravel Soares <88108562+Ravel-Soares@users.noreply.github.com>
2022-12-22 16:02:48 -0300

    ...

--- index.js
+++ index.js
@@ -8,11 +8,17 @@ function animatedToggle(){
     toggle.classList.toggle('active')
 }
 
+if (content1.style.display == 'block'){
+    step1.classList.toggle('activate')
+}
+if (content2.style.display == 'block'){
+    step1.classList.toggle('activate')
+}
 
+document.querySelector('#step2').classList.add('activated')
 
 function navigation(tab1 , tab2){
     document.querySelector(tab1).style.display = 'none';
     document.querySelector(tab2).style.display = 'block';
-
-
 }
+
